b48fb811e407499f7c507240b0b44d03437de830
bala-amypo <bala@amypo.in>
2025-12-17 09:32:36 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/newfileserviceimpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/newfileserviceimpl.java
@@ -3,30 +3,33 @@ import java.util.*;
 import org.springframework.stereotype.Service;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
+
 @Service
 public class StudentServiceImpl implements StudentService{
 
     private final NewFileRepo rep;
 
-    public New
+    public NewfileServiceImpl(NewfileRepo rep){
+        this.rep=rep;
+    }
     @Override
     public StudentValidation savedata(StudentValidation st){
         return rep.save(newfile);
     }
     @Override
     public StudentValildation getidval(Long id){
-
+        return rep.get(id);
     }
     @Override
     public List<StudentValidation> getall(){
-
+        return rep.get();
     }
     @Override
     public Student update(Long id,StudentEntity st){
-
+        return rep.update(id,st)
     }
     @Override
     public void delete(Long id){
-
+        return rep.remove(id)
     }
 }
