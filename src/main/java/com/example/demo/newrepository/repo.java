4d207d3c112b983d012d0ffba9dcf013df23d962
bala-amypo <bala@amypo.in>
2025-12-18 05:04:59 +0000

    User commit

--- src/main/java/com/example/demo/newrepository/repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.newrepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.newentity.StudentValidation;
-@Repository
-public interface repo extends JpaRepository<newentity,Long>{
-}
